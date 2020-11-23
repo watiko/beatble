b3d8653c996a262f1823af89a30cc569c11f73d7
watiko <service@mail.watiko.net>
2020-11-24 06:03:39 +0900

    fix: binary layout

--- src/keyInput.ts
+++ src/keyInput.ts
@@ -18,16 +18,17 @@ export type KeyInput = {
   pressed: KEY_TYPE[];
 };
 
+let counter = 1;
+
 // layout:
-//   0xAA00BC0D
-//   0xEEEE00BC
-//   0x0DFF
+//   0xAA00BC0DEE
+//   0xAA00BC0DFF
 // AA:   disk rotation (clockwise is positive)
 // B:    sum of B5(0x1), B6(0x2), B7(0x4)
 // C:    sum of B1(0x1), B2(0x2), B3(0x4), B4(0x8)
 // D:    sum of E1(0x1), E2(0x2)
-// EEEE: increment by 2 (little endian?)
-// FF:   increment by 2
+// EE:   started from 1, incremented by 2
+// FF:   started from 2, incremented by 2
 export function inputToData(input: KeyInput): Buffer {
   const disk = input.diskRotation & 0xff;
 
@@ -70,23 +71,21 @@ export function inputToData(input: KeyInput): Buffer {
     }
   }
 
-  const now = Date.now() & 0xffffff;
-  const ee1 = (now & 0xff0000) >> 16;
-  const ee2 = (now & 0x00ff00) >> 8;
-  const ff = now & 0x0000ff;
+  const ee = counter;
+  const ff = counter + 1;
+  counter = (counter + 1) & 0xff;
 
   return Buffer.from([
-    // 0xAA00BC0D
+    // 0xAA00BC0DEE
     disk,
     0x00,
     (b << 4) | c,
     d,
-    // 0xEEEE00BC
-    ee1,
-    ee2,
+    ee,
+    // 0xAA00BC0dFF
+    disk,
     0x00,
     (b << 4) | c,
-    // 0x0DFF
     d,
     ff,
   ]);
